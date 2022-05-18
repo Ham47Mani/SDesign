e43c98c858848159aac99729e7fba6dd6ad8f8ce
Ham47Mani <ham47mani@gmail.com>
2022-05-18 01:39:33 +0100

    Add setting box & color option in website and local storage

--- dist/script/script.js
+++ dist/script/script.js
@@ -11,3 +11,45 @@ setInterval( _ => {
   header.style.backgroundImage = `url(imgs/header/${randomImgs}.jpg)`;
 }, 10000);
 //============================= End Header ===============================
+
+//============================= Start Setting Box ===============================
+//----------------- Open Setting Box -----------------------
+document.querySelector(".setting-box .toggele-setting i").onclick= function () {
+  //--- Toggel Spin class in fontawesome gear
+  this.classList.toggle("fa-spin");
+  //--- Toggle Class open in setting box
+  document.querySelector(".setting-box").classList.toggle("open");
+};
+
+//----------------- Switch Color -----------------------
+const colorsLi = document.querySelectorAll(".option-box .colors-lists li");
+colorsLi.forEach(li => {
+  li.addEventListener("click", (e) => {
+    //- Set Color on root
+    document.documentElement.style.setProperty("--main-color", e.target.dataset.color);
+    //- Set color on local storage
+    localStorage.setItem("color_option", e.target.dataset.color);
+    //- Remove class active fromall chrildrens
+    e.target.parentElement.querySelectorAll(".active").forEach(element => {
+      element.classList.remove("active");
+    });
+    e.target.classList.add("active");
+  });
+});
+
+//----------------- Check Local Storage -----------------------
+//- Check if there's local storage color option
+let mainColor = localStorage.getItem("color_option");
+if (mainColor != null) {
+  //- Set Color on root
+  document.documentElement.style.setProperty("--main-color", mainColor);
+  //- Add class active
+  colorsLi.forEach(element => {
+    element.classList.remove("active");
+    if (element.dataset.color == mainColor) {
+      element.classList.add("active");
+    }
+  });
+}
+
+//============================= End Setting Box ===============================
