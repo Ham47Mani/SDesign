0316a514bbb24db39875f7d172d844656c47437f
Ham47Mani <ham47mani@gmail.com>
2022-05-20 21:29:28 +0100

    random background option box

--- src/script/script.js
+++ src/script/script.js
@@ -5,11 +5,43 @@ let header = document.querySelector("header");
 //--- Variables
 const imgNumbs = 4;
 
-setInterval( _ => {
-  let randomImgs = Math.ceil(Math.random() * imgNumbs);
-  //- Change Background Image
-  header.style.backgroundImage = `url(imgs/header/${randomImgs}.jpg)`;
-}, 10000);
+//- Random Background Option
+let backgrounOpt = true;
+//- Background Interval
+let bgInterval;
+
+randomizeImgs();
+
+//- Check if there's local storage random background Item
+let bgLocalItem = localStorage.getItem("background_option");
+if (bgLocalItem != null) {
+  //- Remove class active from all span
+  document.querySelectorAll(".random-background span").forEach(element => {
+    element.classList.remove("active");
+  });
+  if (bgLocalItem == "true") {
+    backgrounOpt = true;
+    randomizeImgs();
+    document.querySelector(".random-background span.yes").classList.add("active");
+  } else {
+    backgrounOpt = false;
+    clearInterval(bgInterval);
+    document.querySelector(".random-background span.no").classList.add("active");
+  }
+
+}
+
+//--- randomizeImgs Function
+function randomizeImgs () {
+  if (backgrounOpt === true) {
+    bgInterval = setInterval( _ => {
+      let randomImgs = Math.ceil(Math.random() * imgNumbs);
+      //- Change Background Image
+      header.style.backgroundImage = `url(imgs/header/${randomImgs}.jpg)`;
+    }, 10000);
+  }
+}
+
 //============================= End Header ===============================
 
 //============================= Start Setting Box ===============================
@@ -29,11 +61,35 @@ colorsLi.forEach(li => {
     document.documentElement.style.setProperty("--main-color", e.target.dataset.color);
     //- Set color on local storage
     localStorage.setItem("color_option", e.target.dataset.color);
-    //- Remove class active fromall chrildrens
+    //- Remove class active from all chrildrens
     e.target.parentElement.querySelectorAll(".active").forEach(element => {
       element.classList.remove("active");
     });
     e.target.classList.add("active");
+  });
+});
+
+//----------------- Switch Random Background -----------------------
+const randomBg = document.querySelectorAll(".option-box.random-background span");
+randomBg.forEach(span => {
+  span.addEventListener("click", (e) => {
+    //- Remove class active from all chrildrens
+    e.target.parentElement.querySelectorAll(".active").forEach(element => {
+      element.classList.remove("active");
+    });
+    e.target.classList.add("active");
+
+    if (e.target.dataset.background == "yes") {
+      backgrounOpt = true;
+      randomizeImgs();
+      localStorage.setItem("background_option", true);
+      console.log("yes");
+    } else {
+      console.log("no");
+      backgrounOpt = false;
+      clearInterval(bgInterval);
+      localStorage.setItem("background_option", false);
+    }
   });
 });
 
